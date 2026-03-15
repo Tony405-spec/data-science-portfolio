03c466734225e259ec35f19ca229945a0bff6e25
Tony Kenga <kengatony@gmail.com>
2026-03-15 13:41:49 +0300

    Update tests/test_feature_engineering.py with test for text_length_feature

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -1,5 +1,8 @@
-from src.preprocessing import text_length_feature
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from src.preprocessing import text_length_feature
 
 def test_text_length_feature():
     rows = [{"name": "Amy"}, {"name": "Bobby"}]
