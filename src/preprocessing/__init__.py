f8322c9e0a35dee42efe15cdf9e3f099f9a20dc4
Ŧ☉ПY KΣПGΔ <kengatony@gmail.com>
2026-03-28 20:00:09 +0300

    Update preprocessing package documentation

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,5 +1,18 @@
-"""Preprocessing node."""
+"""Preprocessing package.
 
-from .preprocess import preprocess_data, split_data
+from .tools import (
+    handle_missing_values,
+    clean_data,
+    scale_features,
+    create_features,
+    text_length_feature,
+)
 
-__all__ = ["preprocess_data", "split_data"]
+__all__ = [
+    "handle_missing_values",
+    "clean_data",
+    "scale_features",
+    "create_features",
+    "text_length_feature",
+]
+"""
